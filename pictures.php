cc214995e0b6944af4c356b9941e6dd26f61fd08
AJ Gayler <austingayler@gmail.com>
2014-11-30 15:09:29 -0700

    pictures update

--- pictures.php
+++ pictures.php
@@ -80,23 +80,22 @@
 				
 				<?php
 				$folder = '/media/pictures';
-				$filetype = '*.jpg';
+				$filetype = '*.*';
 				$files = glob($folder.$filetype);
 				$count = count($files);
-				 
 				$sortedArray = array();
 				for ($i = 0; $i < $count; $i++) {
-					$sortedArray[date ('YmdHis', filemtime($files[$i]))] = $files[$i];
+				$sortedArray[date ('YmdHis', filemtime($files[$i])) . $i] = $files[$i];
 				}
-				 
 				ksort($sortedArray);
+				# krsort($sortedArray);
 				echo '<table>';
-				foreach ($sortedArray as &$filename) {
-					#echo '<br>' . $filename;
-					echo '<tr><td>';
-					echo '<a name="'.$filename.'" href="#'.$filename.'"><img src="'.$filename.'" /></a>';
-					echo substr($filename,strlen($folder),strpos($filename, '.')-strlen($folder));
-					echo '</td></tr>';
+				foreach ($sortedArray as $filename) {
+				echo '<tr><td>';
+				echo '<a name="'.$filename.'" href="#'.$filename.'"><img src="'.$filename.'" /></a>';
+				echo "lol";
+				echo substr($filename,strlen($folder),strpos($filename, '.')-strlen($folder));
+				echo '</td></tr>';
 				}
 				echo '</table>';
 				?>
