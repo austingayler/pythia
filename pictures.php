00b4a8d8e649bdcf88b3b58ff3d04c878a70b22b
AJ Gayler <austingayler@gmail.com>
2014-11-30 15:05:58 -0700

    added pictures page

---
+++ pictures.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Pythia</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="css/logo-nav.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand">
+                    <img height="60" width="90" src="img/pythia-logo.png" alt="">
+                </a>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <li>
+                        <a href="index.php">Pythia</a>
+                    </li>
+                    <li>
+                        <a href="lr.php">Living Room Audio</a>
+                    </li>
+                    <li>
+                        <a href="radio.php">Radio</a>
+                    </li>
+                    <li>
+                        <a href="camera.php">Camera Stream</a>
+                    </li>
+                    <li>
+                        <a href="pictures.php">Pictures</a>
+                    </li>
+                    <li>
+                        <a href="about.php">About</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
+
+    <!-- Page Content -->
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <h1>some pix yo</h1>
+                <p>
+				
+				<?php
+				$folder = '/media/pictures';
+				$filetype = '*.jpg';
+				$files = glob($folder.$filetype);
+				$count = count($files);
+				 
+				$sortedArray = array();
+				for ($i = 0; $i < $count; $i++) {
+					$sortedArray[date ('YmdHis', filemtime($files[$i]))] = $files[$i];
+				}
+				 
+				ksort($sortedArray);
+				echo '<table>';
+				foreach ($sortedArray as &$filename) {
+					#echo '<br>' . $filename;
+					echo '<tr><td>';
+					echo '<a name="'.$filename.'" href="#'.$filename.'"><img src="'.$filename.'" /></a>';
+					echo substr($filename,strlen($folder),strpos($filename, '.')-strlen($folder));
+					echo '</td></tr>';
+				}
+				echo '</table>';
+				?>
+				
+				</p>
+            </div>
+        </div>
+    </div>
+    <!-- /.container -->
+
+    <!-- jQuery Version 1.11.0 -->
+    <script src="js/jquery-1.11.0.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
