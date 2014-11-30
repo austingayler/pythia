1cb6efd41bc3640e6fec420a57ae0a24c13960ca
AJ Gayler <austingayler@gmail.com>
2014-11-30 13:01:12 -0700

    stats edit

--- scripts/system-stats.py
+++ scripts/system-stats.py
@@ -1,35 +1,67 @@
 #!/usr/bin/env python
+import subprocess
+import os
+def get_ram():
+    “Returns a tuple (total ram, available ram) in megabytes. See www.linuxatemyram.com”
+    try:
+        s = subprocess.check_output([“free”,”-m”])
+        lines = s.split(‘\n’)       
+        return ( int(lines[1].split()[1]), int(lines[2].split()[3]) )
+    except:
+        return 0def get_process_count():
+    “Returns the number of processes”
+    try:
+        s = subprocess.check_output([“ps”,”-e”])
+        return len(s.split(‘\n’))       
+    except:
+        return 0def get_up_stats():
+    “Returns a tuple (uptime, 5 min load average)”
+    try:
+        s = subprocess.check_output([“uptime”])
+        load_split = s.split(‘load average: ‘)
+        load_five = float(load_split[1].split(‘,’)[1])
+        up = load_split[0]
+        up_pos = up.rfind(‘,’,0,len(up)-4)
+        up = up[:up_pos].split(‘up ‘)[1]
+        return ( up , load_five )       
+    except:
+        return ( ” , 0 )
 
-import psutil
+def get_connections():
+    “Returns the number of network connections”
+    try:
+        s = subprocess.check_output([“netstat”,”-tun”])
+        return len([x for x in s.split() if x == ‘ESTABLISHED’])
+    except:
+        return 0
 
-from flask import Flask
+def get_temperature():
+    “Returns the temperature in degrees C”
+    try:
+        s = subprocess.check_output([“/opt/vc/bin/vcgencmd”,”measure_temp”])
+        return float(s.split(‘=’)[1][:-3])
+    except:
+        return 0
 
-app = Flask(__name__)
-#app.debug = True # Uncomment to debug
+def get_ipaddress():
+    “Returns the current IP address”
+    arg=’ip route list’
+    p=subprocess.Popen(arg,shell=True,stdout=subprocess.PIPE)
+    data = p.communicate()
+    split_data = data[0].split()
+    ipaddr = split_data[split_data.index(‘src’)+1]
+    return ipaddr
 
-@app.route('/')
-def home():
-    return 'System Stats!'
+def get_cpu_speed():
+    “Returns the current CPU speed”
+    f = os.popen(‘/opt/vc/bin/vcgencmd get_config arm_freq’)
+    cpu = f.read()
+    return cpu
 
-@app.route('/cpu')
-def cpu():
-    return str(psutil.cpu_percent()) + '%'
-
-@app.route('/memory')
-def memory():
-    memory = psutil.virtual_memory()
-    # Divide from Bytes -> KB -> MB
-    available = round(memory.available/1024.0/1024.0,1)
-    total = round(memory.total/1024.0/1024.0,1)
-    return str(available) + 'MB free / ' + str(total) + 'MB total ( ' + str(memory.percent) + '% )'
-
-@app.route('/disk')
-def disk():
-    disk = psutil.disk_usage('/')
-    # Divide from Bytes -> KB -> MB -> GB
-    free = round(disk.free/1024.0/1024.0/1024.0,1)
-    total = round(disk.total/1024.0/1024.0/1024.0,1)
-    return str(free) + 'GB free / ' + str(total) + 'GB total ( ' + str(disk.percent) + '% )'
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+print ‘Free RAM: ‘+str(get_ram()[1])+’ (‘+str(get_ram()[0])+’)’
+print ‘Nr. of processes: ‘+str(get_process_count())
+print ‘Up time: ‘+get_up_stats()[0]
+print ‘Nr. of connections: ‘+str(get_connections())
+print ‘Temperature in C: ‘ +str(get_temperature())
+print ‘IP-address: ‘+get_ipaddress()
+print ‘CPU speed: ‘+str(get_cpu_speed())
